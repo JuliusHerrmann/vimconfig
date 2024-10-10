84e56cadbe2c64e4f818ce651595187cbfda57c3
JuliusHerrmann <herrmann.julius@gmx.de>
2024-10-10 14:10:36 +0200

    markdown stuff + vimtex keybindings

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,11 +1,9 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    config = function ()
-        -- This is a hack to make the plugin work with the current version of neovim
-    end,
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
 }
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,9 +1,14 @@
 return {
-  "lervag/vimtex",
-    ft = {"tex", "bib"},
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
-  end
+	"lervag/vimtex",
+	ft = { "tex", "bib" },
+    keys = {
+        {"<leader>ll", "<cmd>VimtexCompile<cr>"},
+        {"<leader>lc", "<cmd>VimtexClean<cr>"},
+        {"<leader>lv", "<cmd>VimtexView<cr>"}
+    },
+	-- tag = "v2.15", -- uncomment to pin to a specific release
+	init = function()
+		-- VimTeX configuration goes here, e.g.
+		vim.g.vimtex_view_method = "zathura"
+	end,
 }
