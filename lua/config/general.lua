9646133857a3b58211b90b69517b0aae2984433c
JuliusHerrmann <herrmann.julius@gmx.de>
2024-10-17 17:48:35 +0200

    updates + transperancy

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -58,3 +58,5 @@ vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 1
 vim.opt.foldnestmax = 4
+vim.opt.foldcolumn = '0'
+vim.opt.foldenable = true
--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,11 +1,14 @@
 return {
 	"olimorris/onedarkpro.nvim",
 	lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-        style = 'dark'
-    },
-    config = function()
-        vim.cmd([[colorscheme onedark]])
-    end,
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require("onedarkpro").setup({
+			options = {
+                style = "dark",
+				transparency = true,
+			},
+		})
+		vim.cmd([[colorscheme onedark]])
+	end,
 }
