6bce3392d90edc943627d8da40ce223c8906b22b
JuliusHerrmann <herrmann.julius@gmx.de>
2025-05-13 00:34:09 +0200

    fix hor. alignment

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,6 +7,10 @@ return {
 		"L3MON4D3/LuaSnip",
 		"onsails/lspkind.nvim",
 		"vim-tree/nvim-web-devicons",
+		{
+			"windwp/nvim-autopairs",
+			opts = { disable_filetype = { "TelescopePrompt", "vim" } },
+		},
 	},
 
 	-- use a release tag to download pre-built binaries
@@ -79,16 +83,20 @@ return {
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 0,
+				window = {
+					border = "rounded",
+				},
 			},
 			menu = {
+				-- border = "single",
 				draw = {
 					-- columns = {
 					-- 	{ "kind_icon", "label", "label_description", gap = 1 },
 					-- 	{ "kind" },
 					-- },
 					columns = {
-						{ "label", "label_description", gap = 1 },
-						{ "kind_icon", "kind", gap = 1 },
+						{ "kind_icon" },
+						{ "label", "label_description", "kind", gap = 1 },
 					},
 					components = {
 						kind_icon = {
@@ -106,20 +114,6 @@ return {
 								end
 
 								return icon .. ctx.icon_gap
-							end,
-
-							-- Optionally, use the highlight groups from nvim-web-devicons
-							-- You can also add the same function for `kind.highlight` if you want to
-							-- keep the highlight groups in sync with the icons.
-							highlight = function(ctx)
-								local hl = ctx.kind_hl
-								if vim.tbl_contains({ "Path" }, ctx.source_name) then
-									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-									if dev_icon then
-										hl = dev_hl
-									end
-								end
-								return hl
 							end,
 						},
 					},
